6a5e80191c0edab2e5eeef0b4779a40fdfb11d02
Markos Alves pereira <101573252+Markosalves12@users.noreply.github.com>
2022-11-13 00:45:54 -0300

    Add files via upload

---
+++ choroplet_brazil.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import json
+
+caminho_geo_json = r"C:\Dashboardcovid19\geojson\brasil_estados.json"
+caminho_geo_json = caminho_geo_json.replace("\\", "/")
+
+caminho_df = r"C:\Dashboardcovid19\Frames\estadosxdespesas.csv"
+caminho_df = caminho_df.replace("\\", "/")
+
+df = pd.read_csv(caminho_df, encoding="latin-1", delimiter=",")
+geojson = json.load(open(caminho_geo_json))
+
+fig = px.choropleth(df, geojson=geojson, locations='uf', color='valor',  # color_continuous_scale='blues',
+                    scope='south america', projection='natural earth', width=700, height=700,
+                    color_discrete_map={'color': 'red'},
+                    template='plotly_dark')
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+fig.update_layout(activeshape_opacity=0.4)
+fig.update_layout(
+    title={
+        'text': "Espectro de volume de contratos por estado",
+        'y': 0.98,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
